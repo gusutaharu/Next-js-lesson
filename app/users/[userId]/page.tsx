214f2df52e76a44eff8bbfef2d194c64097821d0
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-12-17 08:54:43 +0900

    lesson12

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,7 +1,14 @@
-export default function UserPage() {
+interface Props  {
+  params: { userId: string};
+}
+
+
+export default function UserPage(props: Props) {
   return (
     <>
-      <h1 className="text-lg border-b pb-1 mb-1">UserPage</h1>
+      <h1 className="text-lg border-b pb-1 mb-1">
+        UserPage {props.params.userId}
+      </h1>
     </>
   );
 }
