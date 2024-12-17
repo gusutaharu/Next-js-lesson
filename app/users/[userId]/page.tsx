87a4841e538624aec76121de23559469195a5e2e
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2024-12-17 09:13:40 +0900

    lesson13

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 interface Props  {
   params: { userId: string};
 }
@@ -9,6 +11,11 @@ export default function UserPage(props: Props) {
       <h1 className="text-lg border-b pb-1 mb-1">
         UserPage {props.params.userId}
       </h1>
+      <p>
+        <Link href="/" className="text-blue-500 hover:text-blue-700">
+          Go Back
+        </Link>
+      </p>
     </>
   );
 }
